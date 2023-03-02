0d8ea0ee4a468243b1a11e74bb29965c8ba7bc25
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-03-02 18:07:27 +0200

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3001);
