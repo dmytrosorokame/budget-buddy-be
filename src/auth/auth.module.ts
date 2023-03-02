caf5501d388d14128fe1445c8e56d8604d6eccba
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-03-02 15:48:49 +0200

    fix: connection to db

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { User } from './../user/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [AuthService],
   controllers: [AuthController],
 })
