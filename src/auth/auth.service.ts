8e4398a5afa10525889c0a14dcd54317b33cc40a
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-03-02 15:15:13 +0200

    feat: add user entity & add auth dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { User } from './../user/user.entity';
+import { AuthDto } from './dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
+  async login(dto: AuthDto): Promise<string> {
+    const user = await this.userRepo.findOne({ where: { email: dto.email } });
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return 'You loggedIn';
+  }
+}
