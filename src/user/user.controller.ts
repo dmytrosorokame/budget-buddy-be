f32377bcba444d48135a69d93d691f5c239b9628
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-03-02 17:09:11 +0200

    feat: add get me route & provide jwt guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 
+import { GetUser } from './../auth/decorators/get-user.decorator';
+import { JwtGuard } from './../auth/guard/jwt.guard';
+import { User } from './user.entity';
+
+@UseGuards(JwtGuard)
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get('me')
+  getMe(@GetUser() user: User): User {
+    return user;
+  }
+}
