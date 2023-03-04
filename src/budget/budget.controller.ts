5e4f2cbb13316e5d389781f7821b99fe377beb5e
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-03-04 12:05:38 +0200

    feat: add getAllBudgets route

--- src/budget/budget.controller.ts
+++ src/budget/budget.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 
+import { GetUser } from './../auth/decorators/get-user.decorator';
+import { JwtGuard } from './../auth/guard/jwt.guard';
+import { Budget } from './budget.entity';
+import { BudgetService } from './budget.service';
+
+@UseGuards(JwtGuard)
 @Controller('budget')
-export class BudgetController {}
+export class BudgetController {
+  constructor(private budgetService: BudgetService) {}
+
+  @Get()
+  getAll(@GetUser('id') userId: number): Promise<Budget[]> {
+    return this.budgetService.getAll(userId);
+  }
+}
--- src/budget/budget.service.ts
+++ src/budget/budget.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Budget } from './budget.entity';
 
 @Injectable()
-export class BudgetService {}
+export class BudgetService {
+  constructor(@InjectRepository(Budget) private budgetRepo: Repository<Budget>) {}
+
+  async getAll(userId: number): Promise<Budget[]> {
+    const budgets = await this.budgetRepo.find({
+      relations: ['user'],
+      where: {
+        user: {
+          id: userId,
+        },
+      },
+    });
+
+    return budgets;
+  }
+}
